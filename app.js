713ba28ddc10800be353f88decdb95e8f8c434d0
Amin Shazrin <aminshazrin@yahoo.com>
2018-07-25 19:14:26 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var root = {
+    root : './src/views'
+};
+
+app.get('/' , (req,res)=>{
+    res.sendFile('index.html' , root);
+});
+
+app.get('/about' , (req,res)=>{
+    res.sendFile('index.html' , root);
+});
+
+app.post('/fileupload', (req, res) => {
+    var file = req.files;
+    console.log(`File Upload :\n ${JSON.stringify(file,undefined,2)}`);
+});
+
+app.get('*' , (req,res)=>{
+    res.sendFile('notFound.html' , root);
+});
+
+app.listen(port , '0.0.0.0' , function () {
+    console.log(`Server started at ${port}`);
+})
---
+++ src/assets/js/custom.js
@@ -0,0 +1,16 @@
+/// <reference path="../js/dropzone.js" />
+// DOM Ready
+$(function(){
+    var myDropzone = new Dropzone("form#uploadForm" , {url : '/fileupload'});
+
+    Dropzone.options.myDropzone = {
+        paramName : 'fileName',
+        maxFileSize : 200 , // MB
+        accept : (file,done)=> {
+            if(file.name == "Honeywell.png"){
+                done("You got honey, dear ?");
+            }
+            done();
+        }
+    }
+});
